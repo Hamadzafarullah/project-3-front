264ac3a767bc4704c8191434e926013a4fe597d0
sky0815a <sky0815a@gmail.com>
2022-12-08 15:26:32 -0500

    footer, home, navbar, title added

---
+++ components/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <h4>&copy; Travel App</h4>
+    </div>
+  )
+}
---
+++ components/title.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+
+const title = () => {
+  return (
+    <div>
+      <h2>Look for your dream vaccation</h2>
+      <Navbar />
+    </div>
+  );
+}
+export default title
